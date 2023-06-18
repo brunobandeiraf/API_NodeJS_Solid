c22da42bfd5b67c8fc33c845b1f9485a15c2f37a
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-06-18 18:49:26 -0300

    update specs

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -22,7 +22,9 @@ export async function authenticate(
       password,
     })
     const token = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
@@ -31,7 +33,9 @@ export async function authenticate(
     )
 
     const refreshToken = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
